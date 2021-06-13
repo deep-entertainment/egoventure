ae333fb9ddb1862cd94ca41695e221538f475e38
Dennis Ploeger <develop@dieploegers.de>
2021-06-13 14:15:16 +0200

    fix: Allow walk hotspots to be resized smaller. Fixes #9

--- addons/egoventure/nodes/hotspots/walk_hotspot.gd
+++ addons/egoventure/nodes/hotspots/walk_hotspot.gd
@@ -5,7 +5,18 @@ class_name WalkHotspot, "res://addons/egoventure/images/walk_hotspot.svg"
 extends Hotspot
 
 
+# Set the walk hotspot size, if configured
+func _enter_tree():
+	print(rect_size)
+	if rect_size == Vector2(0,0) and ProjectSettings.has_setting(
+			"EgoVenture/hotspots/walk_hotspot_size"
+		):
+		# call the actual resize deferred to override autolayout
+		call_deferred("_set_hotspot_size")
+
+
 # Set the size of the rect
-func _init():
-	if ProjectSettings.has_setting("EgoVenture/hotspots/walk_hotspot_size"):
-		rect_min_size = ProjectSettings.get_setting("EgoVenture/hotspots/walk_hotspot_size")
+func _set_hotspot_size():
+	set_size(
+		ProjectSettings.get_setting("EgoVenture/hotspots/walk_hotspot_size")
+	)
