7d3ff7d6801eb8c847f416410d0f4f17e9446e9b
Dennis Ploeger <develop@dieploegers.de>
2021-06-13 12:27:32 +0200

    fix: Keep walk hotspot size

--- addons/egoventure/nodes/hotspots/walk_hotspot.gd
+++ addons/egoventure/nodes/hotspots/walk_hotspot.gd
@@ -7,11 +7,16 @@ extends Hotspot
 
 # Set the walk hotspot size, if configured
 func _enter_tree():
-	if ProjectSettings.has_setting("EgoVenture/hotspots/walk_hotspot_size"):
+	print(rect_size)
+	if rect_size == Vector2(0,0) and ProjectSettings.has_setting(
+			"EgoVenture/hotspots/walk_hotspot_size"
+		):
 		# call the actual resize deferred to override autolayout
 		call_deferred("_set_hotspot_size")
 
 
 # Set the size of the rect
 func _set_hotspot_size():
-	set_size(ProjectSettings.get_setting("EgoVenture/hotspots/walk_hotspot_size"))
+	set_size(
+		ProjectSettings.get_setting("EgoVenture/hotspots/walk_hotspot_size")
+	)
