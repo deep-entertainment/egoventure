dc6d784f30bf4434f4dc1cfdfea543c220f6abd6
Dennis Ploeger <develop@dieploegers.de>
2021-07-23 21:53:41 +0200

    fix: Fixed global_class loading

--- addons/pluginsenabler/plugin.gd
+++ addons/pluginsenabler/plugin.gd
@@ -1,13 +1,30 @@
 tool
 extends EditorPlugin
 
+func _quit():
+	get_tree().quit(0)
+
 func _enter_tree():
-    var dir = Directory.new()
-    var editor = get_editor_interface()
-    if dir.dir_exists("res://addons/parrot"):
-        editor.set_plugin_enabled("parrot", true)
-    if dir.dir_exists("res://addons/speedy_gonzales"):
-        editor.set_plugin_enabled("speedy_gonzales", true)
-    if editor.is_plugin_enabled("parrot") and editor.is_plugin_enabled("speedy_gonzales"):
-        editor.set_plugin_enabled("egoventure", true)
+	var dir = Directory.new()
+	var editor = get_editor_interface()
+	editor.get_resource_filesystem().connect("filesystem_changed", self, "_quit")
+	editor.get_resource_filesystem().scan_sources()
+	if dir.dir_exists("res://addons/parrot"):
+		var script = ResourceLoader.load("res://addons/parrot/plugin.gd", "GDScript", true)
+		editor.add_child(script.new())
+		editor.set_plugin_enabled("parrot", true)
+	else:
+		printerr("Parrot not found.")
+	if dir.dir_exists("res://addons/speedy_gonzales"):
+		var script = load("res://addons/speedy_gonzales/plugin.gd")
+		editor.add_child(script.new())
+		editor.set_plugin_enabled("speedy_gonzales", true)
+	else:
+		printerr("speedy_gonzales not found.")
+	if editor.is_plugin_enabled("parrot") and editor.is_plugin_enabled("speedy_gonzales"):
+		var script = load("res://addons/egoventure/plugin.gd")
+		editor.add_child(script.new())
+		editor.set_plugin_enabled("egoventure", true)
+	else:
+		printerr("Can not enable egoventure as parrot or speedy_gonzales are not enabled")
 
