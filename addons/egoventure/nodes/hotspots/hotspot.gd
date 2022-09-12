79e9533f99b4138fcd7bfe7becfcf0a32c3a6d3b
Dennis Ploeger <develop@dieploegers.de>
2022-09-12 21:42:28 +0200

    fix: Wrong handling of show_indicator on detail views
    fixes deep-entertainment/issues#27

--- addons/egoventure/nodes/hotspots/hotspot.gd
+++ addons/egoventure/nodes/hotspots/hotspot.gd
@@ -68,7 +68,7 @@ func _process(_delta):
 # Hotspot indicator toggle
 func _input(event):
 	if show_indicator and \
-			not DetailView.is_visible or DetailView.is_a_parent_of(self):
+			(not DetailView.is_visible or DetailView.is_a_parent_of(self)):
 		if event.is_action_pressed("hotspot_indicator"):
 			Speedy.hidden = true
 			_hotspot_indicator.show()
